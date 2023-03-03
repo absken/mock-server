bb41c6c5002b2a0dfe2c3357d8d7faff6eb48aca
absken <102502006+absken@users.noreply.github.com>
2023-03-02 23:19:36 -0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ function authenticate(req, res) {
       res.json({
         status: 'error',
         message: 'Oops! Your username or password is invalid.',
-        data: req,
+        data: req.body,
       });
     }
   }, 2000);
